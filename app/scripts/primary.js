8e3e17d1389b5f14408a5094bb027eb7a38935a7
mlaugh4 <mwlaugh@gmail.com>
2016-10-11 01:30:50 -0700

    finishing upload stuff

--- app/scripts/primary.js
+++ app/scripts/primary.js
@@ -27,12 +27,82 @@ $('input[type=search]').focus( function() {
 });
 
 
-$('.file').click( function(){
-	console.log("hello")
+////////// UPLOAD PROCESS
 
-	$('.resources').fadeOut(1000)
-	$('.title-text grn').addClass('.editable')
+// Attach events to DOM elements when the page loads
+$(window).on("load", function () {
 
+    // We need to use an <input> box to show the file chooser, but <input> boxes are ugly, especially in IE
+    // Instead of showing in <input> box, lets show a button. When the button is clicked simulate a
+    // click on the <input> box so the user can choose model files.
+    $("#selectModelsToLoad").click(function () {
+        // Simulate a click on the hidden <input> box so the file chooser opens
+        $("#modelChooser").click();
+      });
+
+    // When the user selects models display them and show the open in VR button
+    $("#modelChooser").change(function () {
+        // Clear the list of files that were previously shown
+        $(".chosenFiles").empty();
+        $(".welcome").fadeOut(500);
+        $("#updateModelForm").delay(500).fadeIn(500)
+
+        // Show the files that were chosen
+        var models = "";
+        var files = $("#modelChooser")[0].files;
+        for(var fileIndex = 0; fileIndex < files.length; fileIndex++) {
+        	$(".chosenFiles").append("<small>" + files[fileIndex].name + ", " + "</small>");
+        }
+
+        // Show the upload models button
+        // $("#uploadModels").show();
+      });
+
+    // Upload the models to the bosc APIs
+    $("#updateModelForm").submit(function (event) {
+        //disable the default form submission
+        event.preventDefault();
+
+        $("#updateModelForm").fadeOut(500);
+
+        // Because we are using divs to collect
+        // info, we need to addign the values to
+        // the textareas so theyre pulling into formdata
+        var name = $('.divForm[name=name]').html()
+        $('#hiddenName').val(name);
+        var shortDesc = $('.divForm[name=shortDesc]').html()
+        $('#hiddenSDec').val(shortDesc);
+        var longDesc = $('.divForm[name=longDesc]').html()
+        $('#hiddenLDesc').val(longDesc);
+        //grab all form data
+        var formData = new FormData($(this)[0,1,2]);
+
+        var success = function (model) {
+        	console.log("success")
+            // The server returns the metadata about this model that
+            // was saved in the database. Save it on the update form
+            // so we can use it later
+            $(".modelInfo").data("model", model);
+            $(".modelInfo").fadeIn(500);
+            $('.modelInfo > .name').html(name)
+            $('.modelInfo > .issue-title').html(shortDesc)
+            $('.modelInfo > .description').html(longDesc)
+          }
+
+          var error = function (response, ajaxOptions, thrownError) {
+          	alert("error");
+          	console.log("status: " + response.status);
+          }
+
+          var complete = function () {
+            // $(".loading").hide();
+            console.log("complete")
+          }
+
+          var boscApis = new BoscApis(BoscApis.testAuthId);
+          boscApis.mock();
+          boscApis.postModel(formData, success, error, complete);
+
+          return false;
+        });
 });
-
-$('#updateModelForm > div:nth-child(n+2)').
