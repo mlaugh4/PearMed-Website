ac9fc35d88f9af688a9a56b3c6018366076510a7
Ryan James <rcjames1004@gmail.com>
2016-10-23 17:47:26 -0700

    Attach orbit controls after model is loaded
    Orbit controls check the target to orbit around. In our code, we load
    the target dynamically becacuse the url of the model is not static--it's
    constructured in JS based on the model's id and auth id. As a result, we
    need to attach the orbit controls after we load the target so it finds
    it

--- app/scripts/modelViewer.js
+++ app/scripts/modelViewer.js
@@ -1,16 +1,22 @@
 $(window).on("load", function () {
-	if( !Cookies.get('authId') )
-		window.location.href = 'login.html';
+	BoscSettings.ensureAuthId();
 
 	var modelId = getUrlParameter("modelId");
 	if(!modelId)
 		window.location.href = 'index.html';
 
 	// Add an html element for the model we are going to view
+	// in addition to the camera with orbit controls
+	//
+	// Note:
+	//		Camera needs to be added here dynamically so the orbit controls don't attempt
+	//		to search for the target before it exists.
 	$("a-scene").append(
+		"<a-camera orbit-controls target='#target' distance='1' look-controls-enabled=false wasd-controls-enabled=false position='0 0 0' ></a-camera>" +
 		"<a-assets>" +
 			"<a-asset-item id='modelObj' src='" + BoscSettings.apiRoot  + "organModels/" + modelId + "/obj?authId=" + BoscSettings.authId + "'></a-asset-item>" +
 		"</a-assets>" +
+
 		"<a-entity id='target' obj-model='obj: #modelObj;' position='-1 0 1' rotation='0 45 0' width='1' height='1' depth='1'  color='#4CC3D9'></a-entity>"
 	);
 });
